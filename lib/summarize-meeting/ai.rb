649ea76fb00cd289b07afbe1c8b58eff9345af6b
Benjamin Fleischer <github@benjaminfleischer.com>
2023-03-22 20:46:33 -0500

    refactor: turn module into utility function

--- lib/summarize-meeting/ai.rb
+++ lib/summarize-meeting/ai.rb
@@ -1,50 +1,58 @@
+# frozen_string_literal: true
+
+require "openai"
+
 module SummarizeMeeting
   module Ai
-    class OpenAiError < StandardError; end
+    class OpenAIError < StandardError; end
+    extend self
+
     MAX_TOTAL_TOKENS = 8000
     WORDS_PER_TOKEN = 0.75
 
-    @@access_token = ENV["OPENAI_KEY"]
-    @@organization_id = ENV["OPENAI_ORG"]
-
-    def self.client
+    def client
       @client ||= new_client(access_token: access_token, organization_id: organization_id)
     end
 
-    def self.new_client(access_token:, organization_id:)
+    def new_client(access_token:, organization_id:)
       OpenAI::Client.new(access_token: access_token, organization_id: organization_id)
     end
 
-    def self.access_token
-      @@access_token
+    def access_token
+      @access_token ||= ENV.fetch("OPENAI_KEY") do
+        fail KeyError, "Set OPENAI_KEY in the the ENV or set access_token directly"
+      end
     end
 
-    def self.organization_id
-      @@organization_id
+    def organization_id
+      @organization_id ||= ENV.fetch("OPENAI_ORG") do
+        fail KeyError, "Set OPENAI_ORG in the the ENV or set organization_id directly"
+      end
     end
 
-    def self.access_token=(token)
-      @@access_token = token
+    def access_token=(token)
+      @access_token = token
     end
 
-    def self.organization_id=(id)
-      @@organization_id = id
+    def organization_id=(id)
+      @organization_id = id
     end
 
-    def self.calculate_token_word_count(token_count)
+    def calculate_token_word_count(token_count)
       (token_count * WORDS_PER_TOKEN.to_f).ceil
     end
 
-    def self.calculate_word_token_count(word_count)
+    def calculate_word_token_count(word_count)
       (word_count / WORDS_PER_TOKEN.to_f).ceil
     end
 
-    def self.chat(messages, client: self.client)
+    def chat(messages, client: self.client)
       parameters = {
         model: "gpt-4",
         messages: messages,
       }
       response = client.chat(parameters: parameters)
+
       content = response.dig("choices", 0, "message", "content")
       if !content
         raise OpenAiError, "No response from OpenAI"
